525ca6726563f6a6c0022e8fcfb9d29da94a5058
y.zz <yz0437@gmail.com>
2015-11-25 19:21:44 +0800

    mixin - 对象混合

--- components/animal/Dog.php
+++ components/animal/Dog.php
@@ -1,15 +1,15 @@
 <?php
 	namespace app\components\animal;
-	use app\behaviors\Behavior1;
+// 	use app\behaviors\Behavior1;
 	use yii\base\Component;
 	
 	class Dog extends Component{
-		public function behaviors()	
-		{
-			return [
-				Behavior1::className()
-			];
-		}
+// 		public function behaviors()	
+// 		{
+// 			return [
+// 				Behavior1::className()
+// 			];
+// 		}
 		function look() {
 // 			echo Behavior1::className();
 			echo 'i am looking<br>';
--- controllers/AnimalController.php
+++ controllers/AnimalController.php
@@ -3,17 +3,22 @@
 	use yii\web\Controller;
 	use \yii\base\Event;
 	use app\components\animal\Dog;
-
+	use app\behaviors\Behavior1;
+	
 	class AnimalController extends Controller{
 		public function actionIndex()
 		{
 			
 			$dog = new Dog();
-			$dog->look();
-// 			echo $dog->height;//@see Componet::__get  本类的属性，行为类属性
+			$behavior1 = new Behavior1();
+			$dog->attachBehavior('beh1', $behavior1);
+// 			$dog->detachBehavior('beh1'); //解绑行为
 			$dog->eat();
-			echo '======================event<br>';
-			$dog->trigger('wang');
+// 			$dog->look();
+// // 			echo $dog->height;//@see Componet::__get  本类的属性，行为类属性
+// 			$dog->eat();
+// 			echo '======================event<br>';
+// 			$dog->trigger('wang');
 			
 			
 		}
